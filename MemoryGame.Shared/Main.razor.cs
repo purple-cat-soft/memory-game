9ae3106193f4f10f6ffd137f51c026311b47377b
Cristian Florin Zlatea <cristian.florin@zlatea.com>
2022-05-13 19:40:50 +0200

    Fixed MAUI application

--- MemoryGame.Shared/Main.razor.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MemoryGame.Shared
-{
-  // All the code in this file is included in all platforms.
-  public class Main
-  {
-  }
-}
--- MemoryGame.Shared/Platforms/Tizen/PlatformClass1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace MemoryGame.Shared
-{
-  // All the code in this file is only included on Tizen.
-  public class PlatformClass1
-  {
-  }
-}
